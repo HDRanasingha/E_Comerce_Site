810ece489534ea8bdaece18e79c58f9b7d3fcc97
HDRanasingha <138963111+HDRanasingha@users.noreply.github.com>
2025-06-01 00:38:00 +0530

    Added Repository

--- my-app/src/app/Pages/admin/login/login.component.ts
+++ my-app/src/app/Pages/admin/login/login.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginObj = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private router: Router) {}
+
+  onLogin() {
+    if (this.loginObj.email === 'admin@gmail.com' && this.loginObj.password === '2001') {
+      this.router.navigateByUrl('/products');
+    } else {
+      alert('Wrong Credentials');
+    }
+  }
 }
--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/admin/login/login.component';
+// Import your ProductsComponent or other components as needed
+import { ProductsComponent } from './Pages/admin/products/products.component';
+import { LayoutComponent } from './Pages/admin/layout/layout.component';
 
 export const routes: Routes = [
-    {
-        path:'',
-        redirectTo:'login' ,
-        pathMatch:'full'
-    },
-    {
-        path:'login' ,
-        component:LoginComponent
-    }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'products', component: LayoutComponent},
+ 
+  // ...add other routes as needed
 ];
